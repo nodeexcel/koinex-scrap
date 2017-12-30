de7f51e2ddc172bf840f7910869c90ca3d354ffe
ashutosh_m <ashutosh_m@excellencetechnologies.in>
2017-12-30 12:23:29 +0530

    for separate collections

--- index.js
+++ index.js
@@ -6,6 +6,171 @@ var phantomjs = require('phantomjs');
 var moment = require('moment-timezone');
 var cron = require('node-cron');
 
+
+router.get('/get_btc/:start_date/:end_date', function(req, res, next) {
+    db.BTC_data.find({
+        date: {
+            $gte: req.params.start_date,
+            $lt: req.params.end_date
+        }
+    }).exec(function(err, data) {
+        if (err) {
+            console.log(err)
+            next(err);
+        } else if (data) {
+            var result = data.filter(function(el, index) {
+                return index % 2 === 0;
+            });
+            res.json({ error: 0, message: "data found", data: result });
+        }
+    })
+})
+
+router.get('/get_eth/:start_date/:end_date', function(req, res, next) {
+    db.ETH_data.find({
+        date: {
+            $gte: req.params.start_date,
+            $lt: req.params.end_date
+        }
+    }).exec(function(err, data) {
+        if (err) {
+            console.log(err)
+            next(err);
+        } else if (data) {
+            var result = data.filter(function(el, index) {
+                return index % 2 === 0;
+            });
+            res.json({ error: 0, message: "data found", data: result });
+        }
+    })
+})
+
+router.get('/get_xrp/:start_date/:end_date', function(req, res, next) {
+    db.XRP_data.find({
+        date: {
+            $gte: req.params.start_date,
+            $lt: req.params.end_date
+        }
+    }).exec(function(err, data) {
+        if (err) {
+            console.log(err)
+            next(err);
+        } else if (data) {
+            var result = data.filter(function(el, index) {
+                return index % 2 === 0;
+            });
+            res.json({ error: 0, message: "data found", data: result });
+        }
+    })
+})
+
+router.get('/get_ltc/:start_date/:end_date', function(req, res, next) {
+    db.LTC_data.find({
+        date: {
+            $gte: req.params.start_date,
+            $lt: req.params.end_date
+        }
+    }).exec(function(err, data) {
+        if (err) {
+            console.log(err)
+            next(err);
+        } else if (data) {
+            var result = data.filter(function(el, index) {
+                return index % 2 === 0;
+            });
+            res.json({ error: 0, message: "data found", data: result });
+        }
+    })
+})
+
+router.get('/get_bch/:start_date/:end_date', function(req, res, next) {
+    db.BCH_data.find({
+        date: {
+            $gte: req.params.start_date,
+            $lt: req.params.end_date
+        }
+    }).exec(function(err, data) {
+        if (err) {
+            console.log(err)
+            next(err);
+        } else if (data) {
+            var result = data.filter(function(el, index) {
+                return index % 2 === 0;
+            });
+            res.json({ error: 0, message: "data found", data: result });
+        }
+    })
+})
+
+router.get('/seperate_collections', function(req, res, next) {
+    db.get_detailed_data.find({ is_processed: { "$exists": false } }).limit(2).exec(function(err, data) {
+        if (err) {
+            console.log(err)
+            next(err);
+        } else if (data) {
+            for (var k in data) {
+                var prices = data[k].calculated[0].price;
+                var change_date = moment(new Date(data[k].calculated[0].date)).tz('Asia/Kolkata').format()
+                for (var a in prices) {
+                    prices[a].date = change_date;
+                    if (prices[a].BTC) {
+                        var separate_prices = { 'BTC': prices[a].BTC, 'volume': prices[a].volume, 'open': prices[a].open, 'low': prices[a].low, 'high': prices[a].high, 'close': prices[a].close, 'date': change_date }
+                        BTC_data = new db.BTC_data(separate_prices);
+                        BTC_data.save(function(err, take) {
+                            if (err) {
+                                console.log(err)
+                            }
+                        })
+                    }
+                    if (prices[a].ETH) {
+                        var separate_prices = { 'ETH': prices[a].ETH, 'volume': prices[a].volume, 'open': prices[a].open, 'low': prices[a].low, 'high': prices[a].high, 'close': prices[a].close, 'date': change_date }
+                        ETH_data = new db.ETH_data(separate_prices);
+                        ETH_data.save(function(err, take) {
+                            if (err) {
+                                console.log(err)
+                            }
+                        })
+                    }
+                    if (prices[a].XRP) {
+                        var separate_prices = { 'XRP': prices[a].XRP, 'volume': prices[a].volume, 'open': prices[a].open, 'low': prices[a].low, 'high': prices[a].high, 'close': prices[a].close, 'date': change_date }
+                        XRP_data = new db.XRP_data(separate_prices);
+                        XRP_data.save(function(err, take) {
+                            if (err) {
+                                console.log(err)
+                            }
+                        })
+                    }
+                    if (prices[a].LTC) {
+                        var separate_prices = { 'LTC': prices[a].LTC, 'volume': prices[a].volume, 'open': prices[a].open, 'low': prices[a].low, 'high': prices[a].high, 'close': prices[a].close, 'date': change_date }
+                        LTC_data = new db.LTC_data(separate_prices);
+                        LTC_data.save(function(err, take) {
+                            if (err) {
+                                console.log(err)
+                            }
+                        })
+                    }
+                    if (prices[a].BCH) {
+                        var separate_prices = { 'BCH': prices[a].BCH, 'volume': prices[a].volume, 'open': prices[a].open, 'low': prices[a].low, 'high': prices[a].high, 'close': prices[a].close, 'date': change_date }
+                        BCH_data = new db.BCH_data(separate_prices);
+                        BCH_data.save(function(err, take) {
+                            if (err) {
+                                console.log(err)
+                            }
+                        })
+                    }
+                }
+                db.get_detailed_data.update({ _id: data[k]._id }, { $set: { 'is_processed': 1 } },
+                    function(err, result) {
+
+                    })
+                if (k == data.length - 1) {
+                    res.json({ error: 0, message: "inserted" });
+                }
+            }
+        }
+    })
+})
+
 router.get('/fetch/koinex', function(req, res, next) {
     db.get_detailed_data.find({}).exec(function(err, data) {
         if (err) {
@@ -103,6 +268,7 @@ function doScrap(URL, callback) {
                     if (err) {
                         console.log(err)
                     } else {
+                        var change_date = moment(new Date(last_added[1].date)).tz('Asia/Kolkata').format()
                         for (var k in last_added[1].price) {
                             if (k < 5) {
                                 //if price before two minutes are less then the price before one minute, then store the price before two minutes as the low, and price before one minute as high
@@ -117,6 +283,47 @@ function doScrap(URL, callback) {
                                 }
                                 //store the open value of data coming in current minute as the closing value for the data before two minute
                                 last_added[1].price[k].close = result.price[k].open;
+                                // last_added[1].price[k].date = change_date;
+                                // if (last_added[1].price[k].BTC) {
+                                //     BTC_data = new db.BTC_data(last_added[1].price[k]);
+                                //     BTC_data.save(function(err, take) {
+                                //         if (err) {
+                                //             console.log(err)
+                                //         }
+                                //     })
+                                // }
+                                // if (last_added[1].price[k].ETH) {
+                                //     ETH_data = new db.ETH_data(last_added[1].price[k]);
+                                //     ETH_data.save(function(err, take) {
+                                //         if (err) {
+                                //             console.log(err)
+                                //         }
+                                //     })
+                                // }
+                                // if (last_added[1].price[k].XRP) {
+                                //     XRP_data = new db.XRP_data(last_added[1].price[k]);
+                                //     XRP_data.save(function(err, take) {
+                                //         if (err) {
+                                //             console.log(err)
+                                //         }
+                                //     })
+                                // }
+                                // if (last_added[1].price[k].LTC) {
+                                //     LTC_data = new db.LTC_data(last_added[1].price[k]);
+                                //     LTC_data.save(function(err, take) {
+                                //         if (err) {
+                                //             console.log(err)
+                                //         }
+                                //     })
+                                // }
+                                // if (last_added[1].price[k].BCH) {
+                                //     BCH_data = new db.BCH_data(last_added[1].price[k]);
+                                //     BCH_data.save(function(err, take) {
+                                //         if (err) {
+                                //             console.log(err)
+                                //         }
+                                //     })
+                                // }
                             }
                         }
                         //get the calculated data to store in db
--- koinex_db.js
+++ koinex_db.js
@@ -14,14 +14,90 @@ var koinex_data = mongoose.Schema({
 
 var calculated_data = mongoose.Schema({
     calculated: { type: Array },
-    // date: { type: String },
 }, {
     collection: 'calculated_data',
     strict: true
 });
+
+var BTC = mongoose.Schema({
+    date: { type: String },
+    close: { type: String },
+    high: { type: String },
+    low: { type: String },
+    open: { type: String },
+    volume: { type: String },
+    BTC: { type: String }
+}, {
+    collection: 'BTC',
+    strict: true
+});
+
+var ETH = mongoose.Schema({
+    date: { type: String },
+    close: { type: String },
+    high: { type: String },
+    low: { type: String },
+    open: { type: String },
+    volume: { type: String },
+    ETH: { type: String }
+}, {
+    collection: 'ETH',
+    strict: true
+});
+
+var XRP = mongoose.Schema({
+    date: { type: String },
+    close: { type: String },
+    high: { type: String },
+    low: { type: String },
+    open: { type: String },
+    volume: { type: String },
+    XRP: { type: String }
+}, {
+    collection: 'XRP',
+    strict: true
+});
+
+var LTC = mongoose.Schema({
+    date: { type: String },
+    close: { type: String },
+    high: { type: String },
+    low: { type: String },
+    open: { type: String },
+    volume: { type: String },
+    LTC: { type: String }
+}, {
+    collection: 'LTC',
+    strict: true
+});
+
+var BCH = mongoose.Schema({
+    date: { type: String },
+    close: { type: String },
+    high: { type: String },
+    low: { type: String },
+    open: { type: String },
+    volume: { type: String },
+    BCH: { type: String }
+}, {
+    collection: 'BCH',
+    strict: true
+});
+
 var get_data = conn.model("get_data", koinex_data);
 var get_desired = conn.model("get_desired", calculated_data);
+var get_BTC = conn.model("get_BTC", BTC);
+var get_ETH = conn.model("get_ETH", ETH);
+var get_XRP = conn.model("get_XRP", XRP);
+var get_LTC = conn.model("get_LTC", LTC);
+var get_BCH = conn.model("get_BCH", BCH);
+
 module.exports = {
     fetch: get_data,
     get_detailed_data: get_desired,
+    BTC_data: get_BTC,
+    ETH_data: get_ETH,
+    XRP_data: get_XRP,
+    LTC_data: get_LTC,
+    BCH_data: get_BCH,
 }
