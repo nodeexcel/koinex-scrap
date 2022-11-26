6f44f7fc2cc47abe67d15e02d1a62dc5e2cae619
nodeexcel <vaibhavexcel.2011@gmail.com>
2022-11-26 17:41:35 +0530

    refactor lib

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
 var db = require('./koinex_db.js');
-var routes = require('./index.js');
+var routes = require('./routes');
 var cors = require('cors');
 var bodyParser = require('body-parser');
+var errorHandler = require('./lib/errorHandler');
 
 app.use(cors({
     exposedHeaders: ["Link"]
@@ -13,18 +14,10 @@ app.use(bodyParser.json({
 }));
 
 app.use(bodyParser.json({ type: 'application/*+json' }));
-
 app.use(bodyParser.raw({ type: 'application/vnd.costom-type' }));
 app.use('/', routes);
 app.use(errorHandler);
 
-function errorHandler(error, req, res, next) {
-    if (error.status) {
-        res.status(error.status).json({ error: error });
-    } else {
-        res.status(400).json({ error })
-    }
-}
-app.listen(6001, function() {
+app.listen(6001, function () {
     console.log("Server started at port number: 6001");
 });
